3fa1562d45e6c5889ba0965f68c9059d8bf9278c
Caleb Ferguson <wcg_elite@yahoo.com>
2014-04-24 14:11:33 -0400

    Create SkyBlockChunkGenerator.java

---
+++ Elite-SkyBlock/Java-YML/SkyBlockChunkGenerator.java
@@ -0,0 +1,18 @@
+/*  1:   */ package us.talabrek.ultimateskyblock;
+/*  2:   */ 
+/*  3:   */ import java.util.Random;
+/*  4:   */ import org.bukkit.World;
+/*  5:   */ import org.bukkit.generator.ChunkGenerator;
+/*  6:   */ 
+/*  7:   */ public class SkyBlockChunkGenerator
+/*  8:   */   extends ChunkGenerator
+/*  9:   */ {
+/* 10:   */   public byte[] generate(World world, Random random, int cx, int cz)
+/* 11:   */   {
+/* 12:13 */     byte[] result = new byte[32768];
+/* 13:   */     
+/* 14:   */ 
+/* 15:   */ 
+/* 16:17 */     return result;
+/* 17:   */   }
+/* 18:   */ }
